932a83f9e37bcb18e2990fa505a924085bab7030
Harrison Whetzel <harrisonwhetzel@gmail.com>
2024-03-15 14:54:54 -0400

    Edited app.js file by organizing it as well as commenting on the functions. Also added middleware for session use as well as added error responses

--- app.js
+++ app.js
@@ -3,27 +3,60 @@ const config = require("./config.sample.js");
 const mongoose = require("mongoose");
 const mainRoutes = require('./routes/mainRoutes');
 
-const uri = config.mongoURI;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
-
+//create app
 const app = express();
 
+//configure app
+const uri = config.mongoURI;
 const host = "localhost";
 const port = 3000;
-
 app.set("view engine", "ejs");
+
+//connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(port, host, ()=>{
+        console.log(`Server running at http://localhost:`, port);
+        });
+    })
+    .catch(err=>console.log(err.message));
+
+//mount middleware
+app.use(
+    session({
+        secret: "ajfeirf90aeu9eroejfoefj",
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore({mongoUrl: uri}),
+        cookie: {maxAge: 60*60*1000},
+        
+        })
+);
+
+
+
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+//set up connection to routes
 app.use('/', mainRoutes);
 
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+//set up error responses
+app.use((req, res, next)=>{
+    let err = new Error('The server cannot locate '+ req.uri);
+    err.status = 404;
+    //next(err) calls the next error handler
+    next(err);
+});
+
+app.use((err, req, res, next)=>{
+    console.log(err.stack);
+    if(!err.status){
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+
+    res.status(err.status);
+    res.render('error', {error: err});
 });
