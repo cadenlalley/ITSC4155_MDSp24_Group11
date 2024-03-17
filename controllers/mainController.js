2ad8a4f20898e99820291203c0ef1a62c6703426
Harrison Whetzel <harrisonwhetzel@gmail.com>
2024-03-17 17:48:11 -0400

    added commented code that can be uncommented once user schema is made. Added javascript to ejs so that  the friends, challenges, and groups nav links only show when logged in

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,10 +3,12 @@ const model = require('../models/user');
 //renders the index.ejs file
 exports.index = (req, res) => {
     let activePage = 'home';
+    //uncomment once user model is added
     /*let id = req.session.user;
     model.findById(id)
     .then(user=>{
-      res.render("index", {user});
+      res.render("index", {user, activePage});
     })*/
+    //delete once user model is added
     res.render("index", { activePage });
 };
