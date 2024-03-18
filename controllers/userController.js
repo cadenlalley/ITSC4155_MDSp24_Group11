59161eef6a673252bfd3c94beffa6301fd0d3f08
Caden <cadenlalley@gmail.com>
2024-03-17 20:59:41 -0400

    Add partials to the login/signup pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,43 @@
 const User = require('../models/user');
 // const bcrypt = require('bcrypt');
 
-
 exports.showSignup = (req, res) => {
-    res.render('./user/signup');
+    const activePage = 'signup';
+
+    res.render('./user/signup', { activePage });
 };
 
 exports.showLogin = (req, res) => {
-  res.render('./user/login');
+    const activePage = 'login';
+
+    res.render('./user/login', { activePage });
 };
 
 exports.signup = async (req, res) => {
-  const { username, firstName, lastName, email, password } = req.body;
-  // const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ username, firstName, lastName, email, password });
-  console.log(user);
+    const { username, firstName, lastName, email, password } = req.body;
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, firstName, lastName, email, password });
+    console.log(user);
 
-  try {
-    await user.save();
-    res.status(201).json({ message: 'User created successfully', user });
-    res.redirect('./user/login');
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+    try {
+        await user.save();
+        res.status(201).json({ message: 'User created successfully', user });
+        res.redirect('./user/login');
+    } catch (error) {
+        res.status(500).json({ error });
+    }
 };
 
 exports.login = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
-  if (!user) {
-    return res.status(400).json({ message: 'Invalid email or password' });
-  }
-  // const isMatch = await bcrypt.compare(password, user.password);
-  if(password !== user.password){
-    return res.status(400).json({ message: 'Invalid email or password' });
-  }
-  res.status(200).json({ message: 'Logged in successfully', user });
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    // const isMatch = await bcrypt.compare(password, user.password);
+    if (password !== user.password) {
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    res.status(200).json({ message: 'Logged in successfully', user });
 };
