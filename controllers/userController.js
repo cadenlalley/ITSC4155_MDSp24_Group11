b6bf98f5880d57ca5934dc79cdff987e4507dec1
gvivero1 <gvivero1@uncc.edu>
2024-03-17 20:13:19 -0400

    render login and fixed login method

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,10 @@ exports.showSignup = (req, res) => {
     res.render('./user/signup');
 };
 
+exports.showLogin = (req, res) => {
+  res.render('./user/login');
+};
+
 exports.signup = async (req, res) => {
   const { username, firstName, lastName, email, password } = req.body;
   // const hashedPassword = await bcrypt.hash(password, 10);
@@ -15,22 +19,21 @@ exports.signup = async (req, res) => {
   try {
     await user.save();
     res.status(201).json({ message: 'User created successfully', user });
-    res.redirect('/user/login');
+    res.redirect('./user/login');
   } catch (error) {
     res.status(500).json({ error });
   }
 };
 
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
 
   if (!user) {
     return res.status(400).json({ message: 'Invalid email or password' });
   }
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
+  // const isMatch = await bcrypt.compare(password, user.password);
+  if(password !== user.password){
     return res.status(400).json({ message: 'Invalid email or password' });
   }
   res.status(200).json({ message: 'Logged in successfully', user });
