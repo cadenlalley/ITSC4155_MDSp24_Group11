b58092b5b8e840d8d2aea232b6a7e16e5ab9acf2
gvivero1 <gvivero1@uncc.edu>
2024-03-17 20:04:07 -0400

    fixed some issues on controller and render signup page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,21 @@
-const User = require('./User');
-const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+// const bcrypt = require('bcrypt');
+
+
+exports.showSignup = (req, res) => {
+    res.render('./user/signup');
+};
 
 exports.signup = async (req, res) => {
   const { username, firstName, lastName, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ username, firstName, lastName, email, password: hashedPassword });
+  // const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, firstName, lastName, email, password });
+  console.log(user);
 
   try {
     await user.save();
     res.status(201).json({ message: 'User created successfully', user });
+    res.redirect('/user/login');
   } catch (error) {
     res.status(500).json({ error });
   }
