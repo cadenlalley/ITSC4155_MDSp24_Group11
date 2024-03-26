ca36d60e421c66d04b2dbc07eaa8db37bfc97f00
Harrison Whetzel <harrisonwhetzel@gmail.com>
2024-03-26 19:26:32 -0400

    added connect flash as well as method override. Added javascript to header ejs to show flash messages. Added flash messages for userController as well as implemented a delete account feature.

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const controller = require('../controllers/groupController');
-
 const router = express.Router();
+const groupController = require('../controllers/groupController');
+
 
 // Route to render the group creation page
-router.get('/create', groupController.createGroupPage);
+//router.get('/create', groupController.createGroupPage);
 
 // Route to handle the creation of a new group
-router.post('/create', groupController.createGroup);
+//router.post('/create', groupController.createGroup);
 
 //GET / - goes to index.ejs file in the groupController
-router.get('/', controller.index);
+router.get('/', groupController.index);
 
 module.exports = router;
