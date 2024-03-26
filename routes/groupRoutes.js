4305a9ae877a0558804dccde286fb160689eca12
Michael <djmiemike@gmail.com>
2024-03-26 13:40:55 -0400

    added group creation routes

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -3,6 +3,12 @@ const controller = require('../controllers/groupController');
 
 const router = express.Router();
 
+// Route to render the group creation page
+router.get('/create', groupController.createGroupPage);
+
+// Route to handle the creation of a new group
+router.post('/create', groupController.createGroup);
+
 //GET / - goes to index.ejs file in the groupController
 router.get('/', controller.index);
 
